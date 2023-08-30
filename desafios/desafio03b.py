28403888b89e222c6574fe5382a49555eadc5c6f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-08-30 16:49:55 -0300

    criado aula06a

--- desafios/desafio03b.py
+++ desafios/desafio03b.py
@@ -1,4 +1,5 @@
 n1=int(input('Digite um número: '))
 n2=int(input('Digite outro número: '))
 s=n1+n2
-print('A soma de',n1,'e',n2,'é igual a',s)
+#print('A soma de',n1,'e',n2,'é igual a',s)
+print('A soma de {} e {} é igual a {}'.format(n1,n2,s))
---
+++ pythontestes/aula06a.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um valor: '))
+n2 = int
+print(type(n1))
