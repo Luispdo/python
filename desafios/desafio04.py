ed6e86d79ab4f5d67face3029603f51260516909
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-08-31 15:28:18 -0300

    Criado desafio04.py

---
+++ desafios/desafio04.py
@@ -0,0 +1,8 @@
+x = input('Digite algo: ')
+print(type(x))
+print('O que foi digitado é alfanumérico? ', x.isalnum())
+print('O que foi digitado é alfabético? ', x.isalpha())
+print('O que foi digitado é numérico? ', x.isnumeric())
+print('O que foi digitado está em MAIÚSCULA? ', x.isupper())
+print('O que foi digitado está em minúscula? ', x.islower())
+
