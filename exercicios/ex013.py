bdde5048044fb6d0dea3cf4307286254020d5b8b
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-05 17:21:23 -0300

    Atualizado o ex013.py

--- exercicios/ex013.py
+++ exercicios/ex013.py
@@ -1,4 +1,4 @@
 s = float(input('Digite o salário do funcionário: '))
 i = float(input('Digite o índice da aumento: '))
 a = s + s * (i / 100)
-print('O salário do funcionário, com {:.2f}% de aumento, será de R${:.2f}.'.format(i, a))
+print('O funcionário que recebe R${} de salário, terá um aumento de {:.2f}% e passará a receber um salário de R${:.2f}.'.format(s, i, a))
