6c2a5a5be17bf2c510490db7fb0d0915adb80a4e
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-05 16:57:14 -0300

    Atualizado ex011.py

--- exercicios/ex011.py
+++ exercicios/ex011.py
@@ -3,4 +3,3 @@ h = float(input('Digite a altura da parede em metros: '))
 a = l * h
 qt = a * 2
 print('A quantidade de tinta para se pintar uma parede de {:.2f}m² é de {:.2f} litros.'.format(a, qt))
-
