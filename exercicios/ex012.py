16b2a40d9a2e22a9178bb75d91a05d4c0f8a86e1
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-06 15:45:59 -0300

    Atualizado ex012.py

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,4 +1,4 @@
-p = float(input('Digite o preço do produto: '))
+p = float(input('Digite o preço do produto: R$'))
 d = float(input('Digite a margem de desconto: '))
 np = p - p * (d / 100)
 print('O novo preço do produto, com {}% de desconto é R${:.2f}.'.format(d, np))
