df9bc1559ebc4b99a0034f46b09632d8e897d781
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-06 14:33:29 -0300

    Atualizado ex007.py

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,4 +1,4 @@
 n1 = float(input('Digite uma nota: '))
 n2 = float(input('Digite outra nota: '))
 m = (n1 + n2) / 2
-print('A média das notas {} e {} é {:.2f}'.format(n1, n2, m))
+print('A média das notas {} e {} é {:.1f}'.format(n1, n2, m))
