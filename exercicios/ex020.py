bbf8acb30ee74b2352e1329748a3f7ac47c32e9f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-19 11:21:15 -0300

    Atualizado ex020.py

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -4,5 +4,5 @@ n2 = input('Aluno 2: ')
 n3 = input('Aluno 3: ')
 n4 = input('Aluno 4: ')
 lista = [n1, n2, n3, n4]
-ordem = random.sample(lista, 4)
-print('A ordem de apresentação dos trabalhos será a seguinte: ', ordem)
+random.shuffle(lista)
+print('A ordem de apresentação dos trabalhos será a seguinte: ', lista)
