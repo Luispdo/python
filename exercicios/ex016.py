020e04a062dfe8f8ee603087d04193528923fe5b
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-19 09:40:11 -0300

    Atualizado ex016.py

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,2 +1,3 @@
 n = float(input('Digite um número: '))
-print('O número {} tem a parte inteira {:.0f}.'.format(n, (n//1)))
+'''print('O número {} tem a parte inteira {:.0f}.'.format(n, (n//1)))'''
+print('O número {} tem sua parte inteira é {}'.format(n, int(n)))
