8a0c5992c3a05a9c87f3a2a54cfe40369d72271d
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-19 10:02:35 -0300

    Atualizado ex018.py

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,6 +1,6 @@
 import math
 a = float(input('Digite um ângulo qualquer: '))
-s = math.sin(a)
-c = math.cos(a)
-t = math.tan(a)
+s = math.sin(math.radians(a))
+c = math.cos(math.radians(a))
+t = math.tan(math.radians(a))
 print('Para o ângulo de valor {}°, o seno vale {:.2f}, o cosseno vale {:.2f} e a tangente vale {:.2f}.'.format(a, s, c, t))
