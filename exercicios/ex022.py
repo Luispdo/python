850212f0d08b89691413d92d9b11b4206950e1c0
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-19 16:52:18 -0300

    Criado o ex022.py

---
+++ exercicios/ex022.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome em maúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
