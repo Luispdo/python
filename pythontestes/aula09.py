ea7566ae9aa83ed30994b19bf37de7b8a01ab199
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-19 15:08:29 -0300

    Criada a aula09.py

---
+++ pythontestes/aula09.py
@@ -0,0 +1,7 @@
+frase = 'Curso em Vídeo Python'
+'''print(frase[::2])'''
+print(frase.count('o'))
+frase = frase.replace('Python', 'Android')
+print(frase)
+print('Curso' in frase)
+print(frase.split())
