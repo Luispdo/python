59ff3a5a23463daf01afe5a00045632f3a28b26f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-22 16:28:55 -0300

    Criado ex033.py

---
+++ exercicios/ex033.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+n3 = int(input('Digite o 3º número: '))
+if n1 > n2:
+    nx = n1
+else:
+    nx = n2
+if nx > n3:
+    maior = nx
+else:
+    maior = n3
