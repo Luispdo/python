cf33edaf4224b5ac052711ff1fc35a2599271345
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-22 17:00:23 -0300

    Atualizado ex028.py

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,7 +1,12 @@
-import random
-num = random.randint(0, 5)
+from random import randint
+from time import sleep
+num = randint(0, 5)
+print('-=-' * 20)
 print('Tente acertar o número em que o computador pensou!')
+print('-=-' * 20)
 n = int(input('Digite um número entre 0 e 5: '))
+print('PROCESSANDO...')
+sleep(2)
 print('O número pensado pelo computador é o {}.'.format(num))
 if num == n:
     print('PARABÉN!!! Você acertou em cheio!')
