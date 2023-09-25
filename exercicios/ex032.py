457a71bb1d2dd5976c9875b6449d8b3eff256104
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-25 16:44:23 -0300

    Atualizado ex032.py

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,5 +1,5 @@
-ano = int(input('Entre com um ano qualquer(aaaa): '))
-if ano % 4 == 0:
+ano = int(input('Entre com um ano qualquer: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano de {} é bissexto.'.format(ano))
 else:
     print('O ano de {} não é bisssexto.'.format(ano))
