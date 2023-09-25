619ee457964bc671e98d4bcd648a67a6144d662a
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-25 15:23:22 -0300

    Atualizado ex029.py

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,7 +1,7 @@
-vel = int(input('Digite a velocidade do veículo(Km/h): '))
+vel = float(input('Digite a velocidade do veículo(Km/h): '))
 if vel > 80:
     print('Você foi multado!!!')
     va = vel - 80
-    print('Você estava a {}Km/h acima da velocidade permitida, sua multa será de R${:.2f}.'.format(va, (va * 7)))
+    print('Você estava a {}Km/h acima da velocidade permitida, você será multado em R${:.2f}.'.format(va, (va * 7)))
 else:
-    print('Você não foi multado!')
+    print('Tenha um bom dia! Dirija com segurança!')
