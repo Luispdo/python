a9f844e6b5371beb5e7d412d70db5e4e6fe29f3e
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-25 15:32:06 -0300

    Atualizado ex030.py

--- exercicios/ex030.py
+++ exercicios/ex030.py
@@ -1,6 +1,6 @@
 num = int(input('Digite um número: '))
 if num % 2 == 0:
-    print('O número {} é par.'.format(num))
+    print('O número {} é PAR.'.format(num))
 else:
-    print('O número {} é impar.'.format(num))
+    print('O número {} é ÍMPAR.'.format(num))
     
