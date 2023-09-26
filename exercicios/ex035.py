079fd40282195e4eb0ec58310125f4de16a6fc0f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-26 13:33:58 -0300

    Atualizado ex035.py

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -4,3 +4,8 @@ print('-=' * 15)
 a = float(input('Primeiro segmento: '))
 b = float(input('Segundo segmento: '))
 c = float(input('Terceiro segmento: '))
+if (b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo!')
+    
