a25b5a0473f9e9e5cc9c3ed84b578b303db1b313
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-26 14:34:40 -0300

    Atualizado ex003.py

--- exercicios/ex003.py
+++ exercicios/ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = n1+n2
-print('A soma de {} e {} é igual a {}'.format(n1,n2,s))
+print('A soma de\033[32m {}\033[m e\033[34m {}\033[m é igual a \033[1;4;31;43m{}\033[m'.format(n1,n2,s))
