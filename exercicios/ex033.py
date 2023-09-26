fe80bb5558c424303b3f6d2cdc6fc65120712249
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-26 10:52:44 -0300

    Atualizado ex033.py

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,11 +1,14 @@
 n1 = int(input('Digite o 1º número: '))
 n2 = int(input('Digite o 2º número: '))
 n3 = int(input('Digite o 3º número: '))
-if n1 > n2:
-    nx = n1
-else:
-    nx = n2
-if nx > n3:
-    maior = nx
-else:
+maior = n1
+if n2 > maior:
+    maior = n2
+if n3 > maior:
     maior = n3
+menor = n1
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3
+print('O número MAIOR é igual a {} e o MENOR é igual a {}.'.format(maior, menor))
