4a7a864ef344bb21347f2670eecd28e3b7c787aa
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-26 15:09:17 -0300

    Atualizado ex028.py

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,13 +1,16 @@
 from random import randint
 from time import sleep
+cor = {'limpa':'\033[m', 
+       'verde':'\033[1;32m', 
+       'amarelo':'\033[1;33m'}
 num = randint(0, 5)
-print('-=-' * 20)
-print('Tente acertar o número em que o computador pensou!')
-print('-=-' * 20)
+print('\033[1;32m-=-\033[m' * 20)
+print('\033[1;33mTente acertar o número em que o computador pensou!\33[m')
+print('\033[1;32m-=-\033[m' * 20)
 n = int(input('Digite um número entre 0 e 5: '))
 print('PROCESSANDO...')
 sleep(2)
-print('O número pensado pelo computador é o {}.'.format(num))
+print('O número pensado pelo computador é o {}{}{}.'.format(cor['amarelo'], num, cor['limpa']))
 if num == n:
     print('PARABÉN!!! Você acertou em cheio!')
 else:
