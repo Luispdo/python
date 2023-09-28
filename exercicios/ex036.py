080be6c20eb0ab8c6746ad233d4b54bcbc8ecb69
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-28 17:07:36 -0300

    Atualizado ex036.py

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -3,9 +3,9 @@ sal = float(input('Qual o salário do candidato? R$'))
 prazo = int(input('Em quantos anos será pago? '))
 pmês = prazo * 12
 prestação = valor / pmês
-print(pmês, prestação)
+print('{} prestações de R${:.2f}'.format(pmês, prestação))
 if sal * 0.3 >= valor / pmês:
     print('O empréstimo de R${:.2f} pelo período de {} anos, está \033[1;32mAPROVADO\033[m.'.format(valor, prazo))
     print('O valor da prestação será de R${:.2f} por mês.'.format(pmês))
 else:
-    print('O emprestimo foi \033[1;31mNEGADO\033[m.')
+    print('O empréstimo foi \033[1;31mNEGADO\033[m.')
