709bf6d370c164fb9e4e8e9dbf1a76d59a1772a5
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-09-29 15:54:20 -0300

    Criado ex041.py

---
+++ exercicios/ex041.py
@@ -0,0 +1,14 @@
+from datetime import date
+ano = int(input('Digite o ano de nascimento do atleta: '))
+idade = date.today().year - ano
+if idade <= 9:
+    print('Este atleta tem {} anos, sua categoria é: \033[1;34mMIRIM\033[m'.format(idade))
+elif idade <= 14:
+    print('Este atleta tem {} anos, sua categoria é: \033[1;34mINFANTIL\033[m'.format(idade))
+elif idade <= 19:
+    print('Este atleta tem {} anos, sua categoria é: \033[1;34mJUNIOR\033[m'.format(idade))
+elif idade <= 20:
+    print('Este atleta tem {} anos, sua categoria é: \033[1;34mSÊNIOR\033[m'.format(idade))
+else:
+    print('Este aluno tem {} anos, sua categoria é: \033[1;34mMASTER\033[m'.format(idade))
+    
