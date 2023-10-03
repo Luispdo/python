d6deb1e4c9a47b9062c2c381a89a7f247f8223d3
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-03 16:48:37 -0300

    Atualizado ex042.py

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -1,17 +1,17 @@
-print('-=' * 15)
-print('Analisador de Triângulos')
-print('-=' * 15)
+print('\033[1;32m-=\033[m' * 12)
+print('\033[1;33mAnalisador de Triângulos\033[m')
+print('\033[1;32m-=\033[m' * 12)
 a = float(input('Primeiro segmento: '))
 b = float(input('Segundo segmento: '))
 c = float(input('Terceiro segmento: '))
 if a < b + c and b < a + c and c < a + b:
-    print('Os segmentos acima PODEM FORMAR um triângulo!')
-    if a == b and b == c:
-        print('Do tipo EQUILÁTERO')
+    print('Os segmentos acima PODEM FORMAR um triângulo ', end='')
+    if a == b == c:
+        print('EQUILÁTERO!')
     elif (a == b != c) or (a == c != b) or (b == c != a):
-        print('Do tipo ISÓSCELES')
+        print('ISÓSCELES!')
     else:
-        print('Do tipo ESCALENO')
+        print('ESCALENO!')
 else:
     print('Os segmentos acima NÃO PODEM FORMAR triângulo!')
  
