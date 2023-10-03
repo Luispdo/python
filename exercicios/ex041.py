87fdeb6a4ce2400f228ed4075f8c997ff91868c5
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-03 15:43:13 -0300

    Atualizado ex041.py

--- exercicios/ex041.py
+++ exercicios/ex041.py
@@ -7,7 +7,7 @@ elif idade <= 14:
     print('Este atleta tem {} anos, sua categoria é: \033[1;34mINFANTIL\033[m'.format(idade))
 elif idade <= 19:
     print('Este atleta tem {} anos, sua categoria é: \033[1;34mJUNIOR\033[m'.format(idade))
-elif idade <= 20:
+elif idade <= 25:
     print('Este atleta tem {} anos, sua categoria é: \033[1;34mSÊNIOR\033[m'.format(idade))
 else:
     print('Este aluno tem {} anos, sua categoria é: \033[1;34mMASTER\033[m'.format(idade))
