b49358dda6cacdc62485202d2f4fb9f5fd4ebfb7
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-04 13:36:12 -0300

    Atualizado ex044.py

--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -11,5 +11,13 @@ cpag = int(input('Escolha uma condição de pagamento: '))
 if cpag == 1:
     print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, (preço - preço * 0.1)))
 elif cpag == 2:
-    
-    
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, (preço - preço * 0.05)))
+elif cpag == 3:
+    print('Sua compra vai custar R${:.2f} no final.'.format(preço))
+elif cpag == 4:
+    parcelas = int(input('Quantas parcelas? '))
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS.'.format(parcelas, ((preço + preço * 0.2) / parcelas)))
+    print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, (preço + preço * 0.2)))
+else:
+    print('\033[1;31mOpção Inválida!\033[m')
+           
