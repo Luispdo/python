2591b4642439b9a1ed87808d66f1f7db14d15cef
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-05 17:07:05 -0300

    Atualizado e corrigido ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -13,12 +13,32 @@ jog = int(input('Qual é a sua jogada? '))
 print('JO'), sleep(1)
 print('KEN'), sleep(1)
 print('PÔ!!!'), sleep(1)
-print('\033[1;36m-=\033[m' * 12)
-print('Computador jogou {}'.format(itens[comp]))
-print('Jogador jogou {}'.format(itens[jog]))
-print('\033[1;36m-=\033[m' * 12)
-if comp == jog:
-    print('JOGO EMPATADO!!!')
-elif comp == 0 and jog == 1 or jog == 2:
-    print('JOGADOR VENCEU!!!')
-    elif comp == 1 and jog == 
+if jog != 0 and jog != 1 and jog != 2:
+    print('\033[1;31mJOGADA INVÁLIDA! JOGUE NOVAMENTE...\033[m')
+else:
+    print('\033[1;36m-=\033[m' * 12)
+    print('Computador jogou {}'.format(itens[comp]))
+    print('Jogador jogou {}'.format(itens[jog]))
+    print('\033[1;36m-=\033[m' * 12)
+if comp == 0:
+    if jog == 0:
+        print('\033[1;33mJOGO EMPATADO!\033[m')
+    elif jog == 1:
+        print('\033[1;32mJOGADOR VENCEU!\033[m')
+    elif jog == 2:
+        print('\033[1;34mCOMPUTADOR VENCEU!\033[m')
+elif comp == 1:
+    if jog == 0:
+        print('\033[1;34mCOMPUTADOR VENCEU!\033[m')
+    elif jog == 1:
+        print('\033[1;33mJOGO EMPATADO!\033[m')
+    elif jog == 2:
+        print('\033[1;32mJOGADOR VENCEU!\033[m')
+elif comp == 2:
+    if jog == 0:
+        print('\033[1;32mJOGADOR VENCEU!\033[m')
+    elif jog == 1:
+        print('\033[1;34mCOMPUTADOR VENCEU!\033[m')
+    elif jog == 2:
+        print('\033[1;33mJOGO EMPATADO!\033[m')
+    
