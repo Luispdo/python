d6324538749dba10391c6c640288791de79c5109
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-05 13:02:34 -0300

    Atualizado ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,5 +1,6 @@
 from time import sleep
 from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
 comp = randint(0, 2)
 print('\033[1;33m=\033[m' * 25)
 print('\033[1;32m=       JO-KEN-PÔ       =\033[m')
@@ -8,9 +9,11 @@ print('''Suas opções:
 [ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA''')
-jog = int(input('Qual é a sua jogada? ')), sleep(1)
+jogador = int(input('Qual é a sua jogada? '))
 print('JO'), sleep(1)
 print('KEN'), sleep(1)
-print('PÔ!!!')
-print('-=' * 12)
-'''print('Computador jogou {}'.format())'''
+print('PÔ!!!'), sleep(1)
+print('\033[1;36m-=\033[m' * 12)
+print('Computador jogou {}'.format(itens[comp]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('\033[1;36m-=\033[m' * 12)
