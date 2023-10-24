d2155972ab28930891d1728093a6e24616ac4e94
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-24 10:47:31 -0300

    Atualizado ex048.py

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -3,4 +3,4 @@ for c in range(0, 500, 3):
     if c % 2 == 1:
         s = s + c
         #print(s)
-print('A soma do números impares entre 1 e 500 é igual a: {}'.format(s))
+print('A soma dos números impares entre 1 e 500 é igual a: {}'.format(s))
