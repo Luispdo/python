e644c3f5238fc4ee08e5dd3714dc14998a2dc772
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-25 12:39:22 -0300

    Atualizado ex053.py

--- exercicios/ex053.py
+++ exercicios/ex053.py
@@ -5,6 +5,7 @@ inverso = junto[:: - 1]
 '''inverso = ''
 for letra in range(len(junto) - 1, -1, -1):
     inverso += junto[letra]'''
+print('O inverso de {} fica {}'.format(junto, inverso))
 if inverso == junto:
     print('Temos um palíndromo!')
 else:
