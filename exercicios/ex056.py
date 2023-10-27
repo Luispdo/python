39993f771529178f3a8850fd2a300315cb9568a4
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-10-27 16:18:47 -0300

    Atualizado ex056.py

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,17 +1,24 @@
 idmedia = 0
 totidade = 0
 idhmv = 0
+hmv = ''
+midf = 0
+tot20 = 0
 for p in range(1, 5):
-    nome = str(input('Digite o nome da {}ª pessoa: '.format(p))).upper()
-    idade = int(input('Digite a idade: '))
-    sexo = str(input('Digite o sexo (M para masculino e F para feminino): ')).upper()
-    totidade = totidade + idade
-    if sexo == 'M':
+    print('----- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: '.format(p))).upper().strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).upper()
+    totidade += idade
+    if p == 1 and sexo == 'M':
         idhmv = idade
-        hmv = 'nome'
-        if idade > idhmv:
+        hmv = nome
+    if sexo == 'M' and idade > idhmv:
             idhmv = idade
-            hmv = 'nome'
-            print(hmv)
+            hmv = nome
+    if sexo =='F' and idade < 20:
+        tot20 += 1       
 idmedia = totidade / 4
-print(idmedia)
+print('A média de idade do grupo é de {} anos.'.format(idmedia))
+print('O homem mais velho do grupo é o {}, com {} anos de idade.'.format(hmv, idhmv))
+print('Este grupo contém {} mulher(es) com menos de 20 anos de idade.'.format(tot20))
