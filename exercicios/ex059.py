1c0517ea7dbe0fa99bc1aaf14d9b42afef93f02e
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-01 15:27:12 -0300

    Criado ex059.py

---
+++ exercicios/ex059.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print('=' * 24)
+print(''' [ 1 ] Somar
+ [ 2 ] Multiplicar
+ [ 3 ] Maior
+ [ 4 ] Novos números
+ [ 5 ] Sair do programa''')
+print('=' * 24)
+menu = int(input('Escolha uma opção do menu: '))
