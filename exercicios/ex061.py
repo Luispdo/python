ee6c5daa443ca1921672fb1483d97dd22112df00
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-08 16:46:04 -0300

    Criado ex061.py

---
+++ exercicios/ex061.py
@@ -0,0 +1,9 @@
+s = 0
+pt = int(input('Digite o primeiro termo da PA: '))
+rz = int(input('Digite a razão da PA: '))
+dc = pt + (10 - 1) * rz # formula para calcular o décimo termo de uma progressão
+while c 
+
+for c in range(pt, dc + rz, rz):
+    print('{} '.format(c), end='→ ')
+print('Acabou!')
