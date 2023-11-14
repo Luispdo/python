17f0b19bec30614ae2f828e68ef03567f6f4ed71
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-14 14:12:34 -0300

    Atualizado ex064.py

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -2,9 +2,9 @@ num = 0
 soma = 0
 ct = 0
 print('Entre com alguns valores para obter sua soma. Para encerrar, digite 999.')
-if num != 999:
-    while num != 999:
-        num = int(input('Digite um valor: '))
-        soma += num
-        ct += 1
-print('Foram digitados {} números e a soma entre eles é igual a {}.'.format(ct - 1, soma))
+num = int(input('Digite um valor: '))
+while num != 999:
+    soma += num
+    ct += 1
+    num = int(input('Digite um valor: '))
+print('Foram digitados {} números e a soma entre eles é igual a {}.'.format(ct, soma))
