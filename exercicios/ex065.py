d911bb1c6d277a0682b048a79c1090a22d34ce51
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-16 15:52:23 -0300

    Atualizado ex065.py

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,30 +1,17 @@
-med = 0
-maior = 0
-menor = 0
-soma = 0
-ct = 0
+med = soma = ct = maior = menor = 0
 fim = 'S'
 while fim not in 'N':
     num = int(input('Digite um número: '))
     fim = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     soma += num
     ct += 1
-    print(soma, ct)
-    maior = num
-    menor = num
-    if maior > num:
-        maior = num
-    elif num < menor:
-        menor = num
-med = soma / ct
-print('Foram digitados {} números e a média foi {}'.format(ct, med))
-
-
-if x == 1:
-        pesomaior = peso
-        pesomenor = peso
+    if ct == 1:
+        maior = menor = num
     else:
-        if pesomaior < peso:
-            pesomaior = peso
-        if pesomenor > peso:
-            pesomenor = peso
+        if num > maior:
+            maior = num
+        elif num < menor:
+           menor = num
+med = soma / ct
+print('Foram digitados {} números e a média foi {:.2f}'.format(ct, med))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
