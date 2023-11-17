4cfa41b92344492a6fbbf744add0f3a9f2725c24
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-17 16:10:04 -0300

    Atualizado ex066.py

--- exercicios/ex066.py
+++ exercicios/ex066.py
@@ -1,8 +1,8 @@
 ct = s = 0
 while True:
-    num = int(input('Digite um número: '))
+    num = int(input('Digite um número (999 para parar): '))
     if num == 999:
         break
     ct += 1
     s += num
-print(f'Foram digitados {ct} números que somados valem {s}.')
+print(f'Foram digitados {ct} números que somados valem {s}!')
