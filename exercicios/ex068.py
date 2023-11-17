30576c39ac0446c335b9120ac2a0147e2c399c9f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-17 17:30:00 -0300

    Atualizado ex068.py

--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -2,8 +2,10 @@ from random import randint
 print('=-' * 15)
 print('VAMOS JOGAR PAR OU íMPAR')
 print('=-' * 15)
+resultado = 0
 computador = randint(0, 10)
-jogada = int(input('Diga um valor (0 a 10): '))
-if jogada < 0 > 10:
+jogador = int(input('Diga um valor (0 a 10): '))
+resultado = jogador + computador
+if jogador < 0 or jogador > 10:
     print('Jogada inválida! Tente novamente.')
-print(f'computador {computador} e jogador {jogada}')
+print(f'Você jogou {jogador} e o computador {computador}. Total de {resultado}, deu ')
