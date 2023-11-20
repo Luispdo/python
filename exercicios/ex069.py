5104466ac604dd291cd3c08db8ed6a73f8f39ae2
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-20 15:08:46 -0300

    Atualizado ex069.py

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -16,7 +16,9 @@ while True:
     continua = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if continua == 'N':
         break
+print('')
 print('====== FIM DO PROGRAMA =======')
 print(f'Total de pessoas com mais de 18 anos: {total18}')
 print(f'Ao todos temos {homens} homens cadastrados')
 print(f'E temos {mulheres20} mulheres com menos de 20 anos')
+print('')
