4ccb5641499225a878a2a2b0f3dfaeef3f5472c5
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-20 14:10:17 -0300

    Atualização ex068.py

--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -11,7 +11,7 @@ while True:
     if jogador < 0 or jogador > 10:
         print('Jogada inválida! Tente novamente.')
         break
-    jogada = str(input('Par ou Ímpar? [P/I]')).strip().upper()[0]
+    jogada = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
     resultado = soma % 2
     if resultado == 0:
         parimpar = 'PAR'
