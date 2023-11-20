af81fb823efa011c7388a18c3baf80d5f6fe645a
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-20 14:33:47 -0300

    Criado ex068b.py

---
+++ exercicios/ex068b.py
@@ -0,0 +1,34 @@
+from random import randint
+print('=-' * 15)
+print('VAMOS JOGAR PAR OU íMPAR')
+print('=-' * 15)
+v = 0
+while True:
+    jogador = int(input('Diga um valor de 0 a 10: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).strip().upper()[0]
+    print('-' * 30)
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total} ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    print('-' * 30)
+    if tipo == 'P':
+        if total % 2 ==0:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print('Vamos jogar novamente...')
+    print('=-' * 15)
+print('=-' * 15)
+print(f'GAME OVER! Você venceu {v} vezes.')
