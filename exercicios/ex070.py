34a21043a37782226405aa681aecadd97bb7ed6c
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-21 16:36:38 -0300

    Atualizado ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -1,5 +1,5 @@
 print('-' * 35)
-print('        LOJA SUPER BARATÃO')
+print('{: ^35}'.format('LOJA SUPER BARATÃO'))
 print('-' * 35)
 totp = menorp = totmil = ct = 0
 while True:
@@ -21,7 +21,9 @@ while True:
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
-print('----------- FIM DO PROGRAMA ------------')
+print('{:-^40}'.format(' FIM DO PROGRAMA '))
+print('')
 print(f'O total da compra foi R${totp:.2f}')
 print(f'Temos {totmil} produtos custando mais de R$1000.00')
 print(f'O produto mais barato foi {itemb} que custa R${menorp:.2f}')
+print('')
