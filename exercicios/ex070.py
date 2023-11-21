ed7d115f785f391095d1fa845e8d835623c98e0b
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-21 16:24:23 -0300

    Atualizado ex070.py

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -1,15 +1,27 @@
 print('-' * 35)
 print('        LOJA SUPER BARATÃO')
 print('-' * 35)
-total = menorp = totmais = 0
+totp = menorp = totmil = ct = 0
 while True:
     item = str(input('Nome do Produto: ')).strip()
     preço = float(input('Preço: R$'))
+    totp += preço
+    ct += 1
     resp = ' '
+    if preço >= 1000:
+        totmil += 1
+    if ct == 1:
+        menorp = preço
+        itemb = ' '
+    else:
+        if preço < menorp:
+            menorp = preço
+            itemb = item
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
-print(f'O total da compra foi R${total:.2f}')
-print(f'Temos {} produtos custando mais de R${:.2f}')
-print(f'O produto mais barato foi {item} que custa R${menorp:.2f}')
+print('----------- FIM DO PROGRAMA ------------')
+print(f'O total da compra foi R${totp:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {itemb} que custa R${menorp:.2f}')
