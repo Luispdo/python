fe36b6fb48ae156db89d3cccc445f89cc4767574
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-21 14:25:06 -0300

    Atualizado ex069.py

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -1,19 +1,21 @@
-total18 = 0
-homens = 0
-mulheres20 = 0
+total18 = homens = mulheres20 = 0
 while True:
     print('-' * 25)
     print('   CADASTRE UMA PESSOA')
     print('-' * 25)
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo: ')).strip().upper()[0]
-    if idade > 18:
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    if idade >= 18:
         total18 += 1
     if sexo == 'M':
         homens += 1
     if sexo == 'F' and idade < 20:
         mulheres20 += 1
-    continua = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    continua = ' '
+    while continua not in 'SN':
+        continua = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if continua == 'N':
         break
 print('')
