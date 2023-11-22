d1404c836e938856f0ae97a8c2b71b77ef3c494a
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-22 08:57:40 -0300

    Atualizado ex071.py

--- exercicios/ex071.py
+++ exercicios/ex071.py
@@ -3,7 +3,7 @@ print('{:^45}'.format('BANCO CEV'))
 print('=' * 45)
 totced = 0
 ced = 50
-valor = int(input('Que valor você quer sacar: R$'))
+valor = int(input('Que valor você quer sacar? R$'))
 total = valor
 while True:
     if total >= ced:
@@ -12,5 +12,14 @@ while True:
     else:
         if totced > 0:
             print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
 print('=' * 45)
 print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
