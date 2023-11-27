ace9ba3368d9b5033c4e4c4be298ba5ca6303507
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-27 14:40:59 -0300

    Atualizado ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,4 +1,8 @@
-times = ('Palmeiras', 'Flamengo', 'Botafogo', 'Atlético-MG', 'Gremio', 'Bragantino', 'Fluminense', 'Atlético-PR', 'Cuiabá', 'São Paulo', 'Internacional', 'Fortaleza', 'Corinthians', 'Santos', 'Vasco da Gama', 'Bahia', 'Cruzeiro', 'Goiás', 'Coritiba', 'América-MG')
+times = ('Palmeiras', 'Flamengo', 'Botafogo', 'Atlético-MG',
+         'Gremio', 'Bragantino', 'Fluminense', 'Atlético-PR',
+         'Cuiabá', 'São Paulo', 'Internacional', 'Fortaleza',
+         'Corinthians', 'Santos', 'Vasco da Gama', 'Bahia',
+         'Cruzeiro', 'Goiás', 'Coritiba', 'América-MG')
 print('-=' * 15)
 print(f'Lista de times do Brasileirão: {times}')
 print('-=' * 15)
@@ -8,5 +12,5 @@ print(f'Os 4 últimos são {times[-4:]}')
 print('-=' * 15)
 print(f'Times em ordem alfabética: {sorted(times)}')
 print('-=' * 15)
-print(f'O Cruzeiro está na {times.index('Cruzeiro')}ª posição')
+print(f'O Cruzeiro está na {times.index("Cruzeiro") + 1}ª posição')
 print('')
