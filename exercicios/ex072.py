7855e4714d50be876987576bfa99807a16fd6b23
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-11-27 14:27:40 -0300

    Atualizado ex072.py

--- exercicios/ex072.py
+++ exercicios/ex072.py
@@ -1,7 +1,8 @@
 numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-num = int(input('Digite um número entre 0 e 20: '))
-while num < 0 or num > 20:
-    print('Tente novamente. ', end='')
+while True:
     num = int(input('Digite um número entre 0 e 20: '))
+    if 0 < num < 20:
+        break
+    print('Tente novamente. ', end='')
 print(f'Você digitou o número {numero[num]}')
     
