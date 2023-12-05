d64ffd3ef95ac864ab217462362f6fbfacb0623e
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-12-05 17:08:32 -0300

    Criado o ex082.py

---
+++ exercicios/ex082.py
@@ -0,0 +1,20 @@
+lista = []
+listapar = []
+listaimpar = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+    
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'A lista completa é {lista}')
+for n in lista:
+    if n % 2 == 0:
+        listapar.append(n)
+    else:
+        listaimpar.append(n)
+print(f'A lista de pares é {listapar}')
+print(f'A lista de impares é {listaimpar}')
+print()
