0e870f47318fcba740bead69846485c521ca4534
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-12-05 15:16:50 -0300

    Atualizado o ex079.py

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -2,14 +2,15 @@ valores = []
 while True:
     n = int(input('Digite um valor: '))
     if n not in valores:
-        valores.append(int(input('Digite um valor: ')))
+        valores.append(n)
         print('Valor adicionado com sucesso...')
     else:
         print('Valor duplicado! Não vou adicionar...')
-    
     resp = ' '    
     while resp not in 'SN':
         resp = str(input('Quer continuar? [SN] ')).strip().upper()[0]
     if resp == 'N':
         break
-print(valores.sort())
+print('-=' * 30)
+valores.sort()
+print(f'Você digitou os valores {valores}')
