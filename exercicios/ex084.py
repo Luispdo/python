f92868e6ccae57cd8eac403c2a1c6c1d036d6799
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-12-28 15:07:36 -0300

    Atualizado o ex084.py

--- exercicios/ex084.py
+++ exercicios/ex084.py
@@ -1,21 +1,32 @@
 pessoas = list()
 dado = list()
-cont = maipeso = menpeso = 0
+maipeso = menpeso = 0
 while True:
     dado.append(str(input('Nome: ')))
-    dado.append(int(input('Peso: ')))
+    dado.append(float(input('Peso: ')))
+    if len(pessoas) == 0:
+        maipeso = menpeso = dado[1]
+    else:
+        if dado[1] > maipeso:
+            maipeso = dado[1]
+        elif dado[1] < menpeso:
+            menpeso = dado[1]
     pessoas.append(dado[:])
     dado.clear()
-    cont += 1
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if resp == 'N':
         break
 print('-=' * 30)
-print(f'Foram cadastradas {cont} pessoas.')
+print(f'Foram cadastradas {len(pessoas)} pessoas.')
+print(f'O maior peso foi de {maipeso:.1f}Kg. Peso de ', end='')
 for p in pessoas:
-    if p[1] > 100:
-        print(f'O maior peso foi de {p[1]:.1f}Kg. Peso de {p[0]}')
-    else:
-        print(f'O menor peso foi de {p[1]:.1f}Kg. Peso de {p[0]}')
+    if p[1] == maipeso:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi de {menpeso:.1f}Kg. Peso de ', end='')
+for p in pessoas:
+    if p[1] == menpeso:
+        print(f'[{p[0]}] ')
+print()
