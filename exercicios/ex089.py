d908ab20982d606c262822b3394eaae9f943b13f
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2024-01-15 12:45:34 -0300

    Criado o ex089.py

---
+++ exercicios/ex089.py
@@ -0,0 +1,16 @@
+ficha = []
+dados = []
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar: [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(ficha)
