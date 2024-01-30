1fb38f174140bdca8eb5b7ff7a7ad2bf22f6b3f4
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2024-01-30 10:58:44 -0300

    Atualizado o ex094.py

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,5 +1,6 @@
 ficha = {}
 dados = []
+soma = média = 0
 while True:
     ficha.clear()
     ficha['nome'] = str(input('Nome: '))
@@ -9,6 +10,7 @@ while True:
             break
         print('ERRO! Por favor, digite apenas M ou F.')
     ficha['idade'] = int(input('Idade: '))
+    soma += ficha['idade']
     dados.append(ficha.copy())
     while True:
         resp = str(input('Quer continuar: [S/N] ')).strip().upper()[0]
@@ -17,4 +19,22 @@ while True:
         print('ERRO! Responda apenas S ou N.')
     if resp == 'N':
         break
-print(dados)
+print('-=' * 30)
+print(f'A) Ao todo temos {len(dados)} pessoas cadastradas.')
+média = soma / len(dados)
+print(f'B) A média de idade é de {média:.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
+for p in dados:
+    if p['sexo'] in 'F':
+        print(f'|{p["nome"]}|', end='')
+print()
+print('D) Lista das pessoas que estão acima da média: ')
+for p in dados:
+    if p['idade'] >= média:
+        print('    ')
+        for k, v in p.items():
+            print(f'   {k} = {v}; ', end='')
+        print()
+print()
+print('<< ENCERRADO >>')
+print()
