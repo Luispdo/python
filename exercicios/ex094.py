29550c7661d5da4afc58f915f010560fd2067ecd
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2024-01-30 10:30:32 -0300

    Atualizado ex094.py

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,16 +1,20 @@
-ficha = []
+ficha = {}
 dados = []
 while True:
-    nome = str(input('Nome: '))
-    sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
-    if sexo not in 'SN':
+    ficha.clear()
+    ficha['nome'] = str(input('Nome: '))
+    while True:
+        ficha['sexo'] = str(input('Sexo: [M/F] ')).strip().upper()[0]
+        if ficha['sexo'] in 'MF':
+            break
         print('ERRO! Por favor, digite apenas M ou F.')
-    idade = int(input('Idade: '))
-    #media = (nota1 + nota2) / 2
-    ficha.append([nome, sexo, idade, media])
-    
-    resp = ' '
-    while resp not in 'SN':
+    ficha['idade'] = int(input('Idade: '))
+    dados.append(ficha.copy())
+    while True:
         resp = str(input('Quer continuar: [S/N] ')).strip().upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
     if resp == 'N':
         break
+print(dados)
